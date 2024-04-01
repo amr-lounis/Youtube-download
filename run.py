e0b665cc03f0413aa7704fd9f9c9859df85ba744
LOUNIS AMAR <56897657+amr-lounis@users.noreply.github.com>
2024-04-01 22:38:00 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,13 @@
 #pip install pytube
-from pytube import Playlist ,YouTube
+from pytube import Playlist, YouTube
 import re
 import os
-
-url_playlist = 'https://www.youtube.com/watch?v=Q0mWz1BYH-k&list=PLUitXL66pnO_K_Q_mqnB7MzoHjLnkU_jo&index=110'
-# res= 360p / 720p / 144 
-res = "720p"
-
-playlist = Playlist(url_playlist)
-listUrl = playlist.video_urls
-
-for i in range(0,len(listUrl)):
+# --------------------------------------------------------------------------------------------------------------------------
+def downloadOne(_url):
+    # res= 360p / 720p / 144 
+    res = "720p"
     try:
-        yt = YouTube(listUrl[i])
+        yt = YouTube(_url)
         video = yt.streams.get_highest_resolution()
         # video =   yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc() .first()
         video =   yt.streams.filter(progressive=True, file_extension='mp4',res=res).first()    
@@ -29,4 +24,17 @@ for i in range(0,len(listUrl)):
             print("----- > : exist : ",title)
             # break
     except:
-        print("error : ",i)
+        print("error : ",i)        
+# --------------------------------------------------------------------------------------------------------------------------
+# url_playlist = 'https://www.youtube.com/watch?v=Q0mWz1BYH-k&list=PLUitXL66pnO_K_Q_mqnB7MzoHjLnkU_jo&index=110'
+# playlist = Playlist(url_playlist)
+# listUrl = playlist.video_urls
+# --------------------------------------------------------------------------------------------------------------------------
+# var scroll = setInterval(function(){ window.scrollBy(0, 1000)}, 1000);
+# window.clearInterval(scroll); console.clear(); urls = $$('a'); urls.forEach(function(v,i,a){if (v.id=="video-title-link"){console.log('\t'+v.title+'\t'+v.href+'\t')}});
+# window.clearInterval(scroll); console.clear(); urls = $$('a'); urls.forEach(function(v,i,a){if (v.id=="video-title-link"){console.log(v.href)}});
+file = open("list.txt", "r")
+listUrl = file.readlines()
+# --------------------------------------------------------------------------------------------------------------------------
+for i in range(0,len(listUrl)):
+    downloadOne(listUrl[i])
