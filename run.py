b2d97dd9104077a5630431f4550340fd1aa2b65c
LOUNIS AMAR <56897657+amr-lounis@users.noreply.github.com>
2024-05-15 22:06:38 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,40 +1,32 @@
 #pip install pytube
 from pytube import Playlist, YouTube
 import re
-import os
 # --------------------------------------------------------------------------------------------------------------------------
 def downloadOne(_url):
     # res= 360p / 720p / 144 
     res = "720p"
+    prefix= "_"
+    extension= "mp4"
     try:
         yt = YouTube(_url)
-        video = yt.streams.get_highest_resolution()
-        # video =   yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc() .first()
-        video =   yt.streams.filter(progressive=True, file_extension='mp4',res=res).first()    
-        # title = str(i) + '-' + video.title
+        # video = yt.streams.get_highest_resolution()
+        video = yt.streams.filter(progressive=True, file_extension=extension,res=res).order_by('resolution').desc() .first()
         title = video.title
         title = re.sub('[\/:?*"<>|]', '_', title)
-
-        print("------------------ > : ",str(i))
-        if not os.path.isdir(title) :
-            print("----- > : ",title)
-            # os.makedirs(title)
-            video.download(title)
-        else:
-            print("----- > : exist : ",title)
-            # break
-    except:
-        print("error : ",i)        
+        video.download(output_path="./download/" ,filename=title+"."+extension ,filename_prefix=prefix )
+    except Exception as e:
+        print("error : ",_url)        
 # --------------------------------------------------------------------------------------------------------------------------
-# url_playlist = 'https://www.youtube.com/watch?v=Q0mWz1BYH-k&list=PLUitXL66pnO_K_Q_mqnB7MzoHjLnkU_jo&index=110'
-# playlist = Playlist(url_playlist)
-# listUrl = playlist.video_urls
+url_playlist = 'https://www.youtube.com/watch?v=TyKwwVemYhw&list=PLKhm8Z5pXdOUWVTnTojfHw_Cr7Ac-HLyR'
+playlist = Playlist(url_playlist)
+listUrl = playlist.video_urls
 # --------------------------------------------------------------------------------------------------------------------------
 # var scroll = setInterval(function(){ window.scrollBy(0, 1000)}, 1000);
 # window.clearInterval(scroll); console.clear(); urls = $$('a'); urls.forEach(function(v,i,a){if (v.id=="video-title-link"){console.log('\t'+v.title+'\t'+v.href+'\t')}});
 # window.clearInterval(scroll); console.clear(); urls = $$('a'); urls.forEach(function(v,i,a){if (v.id=="video-title-link"){console.log(v.href)}});
-file = open("list.txt", "r")
-listUrl = file.readlines()
+# file = open("list.txt", "r")
+# listUrl = file.readlines()
 # --------------------------------------------------------------------------------------------------------------------------
 for i in range(0,len(listUrl)):
+    print("------------------ > : ",str(i))
     downloadOne(listUrl[i])
